e7e0ca5dd19c176d3ba57b5fba8e6ff03f7a27ea
Szymon Grabia <szymon.grabia@gmail.com>
2018-05-29 00:49:06 +0200

    Add 4 approaches for bubble sort

--- BubbleSort/kernel.cu
+++ BubbleSort/kernel.cu
@@ -1,121 +1,324 @@
+﻿#include "cuda_runtime.h"
 
-#include "cuda_runtime.h"
+#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+#include <ctime>
 #include "device_launch_parameters.h"
 
-#include <stdio.h>
+using namespace std;
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-
-__global__ void addKernel(int *c, const int *a, const int *b)
+inline cudaError_t checkCuda(cudaError_t result)
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+	if (result != cudaSuccess) {
+		cout << "CUDA Runtime Error: " << cudaGetErrorString(result) << endl;
+	}
+
+	return result;
+}
+
+__global__ void kernel(float *a, int arraySize, int k)
+{
+	int index = blockIdx.x*blockDim.x + threadIdx.x;
+	int N = 2 * index;
+
+	float tmp;
+
+	if (k%2 == 1) {
+		if (N + 2 < arraySize) {
+			if (a[N + 1] > a[N + 2]) {
+				tmp = a[N + 1];
+				a[N + 1] = a[N + 2];
+				a[N + 2] = tmp;
+			}
+		}
+	}
+	else {
+		if (a[N] > a[N + 1]) {
+			tmp = a[2 * index];
+			a[N] = a[N + 1];
+			a[N + 1] = tmp;
+		}
+	}
+}
+
+__global__ void kernel2(float* dev_tab, unsigned int N)
+{
+	int ind, i, j, s;
+	float a, b;
+	ind = 2 * (threadIdx.x + blockDim.x*blockIdx.x);
+	for (unsigned int k = 0; k < N-1; k++)
+	{
+		s = (k % 2);
+		i = ind + s;
+		j = ind + 1 + s;
+		if (j<N)
+		{
+			a = dev_tab[i];
+			b = dev_tab[j];
+			if (b<a)
+			{
+				dev_tab[i] = b;
+				dev_tab[j] = a;
+			}
+		}
+		__syncthreads();
+	}
+}
+
+__global__ void kernel3(float* dev_tab, unsigned int N, const int Nt)
+{
+	extern __shared__ float stab[];
+	int ind, i, j, s;
+	float a, b;
+	ind = 2 * threadIdx.x;
+
+	i = ind;
+	j = ind + 1;
+	a = dev_tab[i];
+	b = dev_tab[j];
+
+	if (b < a) {
+		stab[i] = b;
+		stab[j] = a;
+	} else {
+		stab[i] = a;
+		stab[j] = b;
+	}
+	__syncthreads();
+
+	for (unsigned int k = 1; k < N-2; k++)
+	{
+		s = (k % 2);
+		i = ind + s;
+		j = ind + 1 + s;
+		if (j<N)
+		{
+			a = stab[i];
+			b = stab[j];
+			if (b<a)
+			{
+				stab[i] = b;
+				stab[j] = a;
+			}
+		}
+		__syncthreads();
+	}
+
+	i = ind;
+	j = ind + 1;
+	a = stab[i];
+	b = stab[j];
+
+	if (b<a) {
+		dev_tab[i] = b;
+		dev_tab[j] = a;
+	} else {
+		dev_tab[i] = a;
+		dev_tab[j] = b;
+	}
+}
+
+void bubbleSortCpu(float* b, int n)
+{
+	int i, j;
+
+	double tt;
+	LARGE_INTEGER tb, te, tf;
+	QueryPerformanceFrequency(&tf);
+	QueryPerformanceCounter(&tb);
+
+	for (i = 0; i < n - 1; i++) {
+		for (j = 0; j < n - i - 1; j++) {
+			if (b[j] > b[j + 1]) {
+				swap(b[j], b[j + 1]);
+			}
+		}
+	}
+
+	QueryPerformanceCounter(&te);
+	tt = 1000.0*(double(te.QuadPart - tb.QuadPart)) / double(tf.QuadPart);
+	cout << "\n\nCPU time: " << tt << " ms\n";
+}
+
+void bubbleSortCuda(float *a, float *b, int arraySize)
+{
+	dim3 threadsPerBlock(arraySize/2);
+	dim3 blocksPerGrid(1);
+
+	float *d_a;
+
+	double tt, tu;
+	LARGE_INTEGER tb, te, tk, tm, tf;
+	QueryPerformanceFrequency(&tf);
+	QueryPerformanceCounter(&tb);
+
+	checkCuda(cudaMalloc((void**)&d_a, arraySize * sizeof(float)));
+	checkCuda(cudaMemcpy(d_a, a, arraySize * sizeof(float), cudaMemcpyHostToDevice));
+
+	QueryPerformanceCounter(&tk);
+	for (int k = 0; k < (arraySize-1); k++) {
+		kernel << <blocksPerGrid, threadsPerBlock >> > (d_a, arraySize, k);
+	}
+	QueryPerformanceCounter(&tm);
+
+	checkCuda(cudaMemcpy(b, d_a, arraySize * sizeof(float), cudaMemcpyDeviceToHost));
+	cudaFree(d_a);
+
+	QueryPerformanceCounter(&te);
+	tu = 1000.0*(double(tm.QuadPart - tk.QuadPart)) / double(tf.QuadPart);
+	cout << "\n\nGPU 1 time: " << tu << " ms\n";
+	tt = 1000.0*(double(te.QuadPart - tb.QuadPart)) / double(tf.QuadPart);
+	cout << "\n\nGPU 1 time with memory operations: " << tt << " ms\n";
+}
+
+void bubbleSortCuda2(float *a, float *b, int arraySize)
+{
+	dim3 threadsPerBlock(arraySize / 2);
+	dim3 blocksPerGrid(1);
+
+	float *d_a;
+
+	double tt, tu;
+	LARGE_INTEGER tb, te, tk, tm, tf;
+	QueryPerformanceFrequency(&tf);
+	QueryPerformanceCounter(&tb);
+
+	checkCuda(cudaMalloc((void**)&d_a, arraySize * sizeof(float)));
+	checkCuda(cudaMemcpy(d_a, a, arraySize * sizeof(float), cudaMemcpyHostToDevice));
+	
+	QueryPerformanceCounter(&tk);
+	kernel2 << <blocksPerGrid, threadsPerBlock >> > (d_a, arraySize);
+	QueryPerformanceCounter(&tm);
+
+	checkCuda(cudaMemcpy(b, d_a, arraySize * sizeof(float), cudaMemcpyDeviceToHost));
+	cudaFree(d_a);
+
+	QueryPerformanceCounter(&te);
+	tu = 1000.0*(double(tm.QuadPart - tk.QuadPart)) / double(tf.QuadPart);
+	cout << "\n\nGPU 2 time: " << tu << " ms\n";
+	tt = 1000.0*(double(te.QuadPart - tb.QuadPart)) / double(tf.QuadPart);
+	cout << "\n\nGPU 2 time with memory operations: " << tt << " ms\n";
+}
+
+void bubbleSortCuda3(float *a, float *b, int arraySize)
+{
+	dim3 threadsPerBlock(arraySize / 2);
+	dim3 blocksPerGrid(1);
+
+	float *d_a;
+
+	double tt, tu;
+	LARGE_INTEGER tb, te, tk, tm, tf;
+	QueryPerformanceFrequency(&tf);
+	QueryPerformanceCounter(&tb);
+
+	checkCuda(cudaMalloc((void**)&d_a, arraySize * sizeof(float)));
+	checkCuda(cudaMemcpy(d_a, a, arraySize * sizeof(float), cudaMemcpyHostToDevice));
+
+	QueryPerformanceCounter(&tk);
+	kernel3 << <blocksPerGrid, threadsPerBlock, arraySize * sizeof(float) >> > (d_a, arraySize, threadsPerBlock.x);
+	QueryPerformanceCounter(&tm);
+	
+	checkCuda(cudaMemcpy(b, d_a, arraySize * sizeof(float), cudaMemcpyDeviceToHost));
+	cudaFree(d_a);
+
+	QueryPerformanceCounter(&te);
+	tu = 1000.0*(double(tm.QuadPart - tk.QuadPart)) / double(tf.QuadPart);
+	cout << "\n\nGPU 3 time: " << tu << " ms\n";
+	tt = 1000.0*(double(te.QuadPart - tb.QuadPart)) / double(tf.QuadPart);
+	cout << "\n\nGPU 3 time with memory operations: " << tt << " ms\n";
 }
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	srand((int)time(nullptr));
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	bool display = true;
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	const int arraySize = 2048;
+    //const int arraySize = 2048;
+	float a[arraySize];
+	float b[arraySize];
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	for (int r = 0; r < arraySize; r++) {
+		a[r] = (rand() % 1000) / 100.0;
+	}
+
+	if (display) {
+		cout << "A" << endl;
+		for (int i = 0; i < arraySize; i++) {
+			cout << a[i] << "\t";
+		}
+		cout << endl;
+	}
+
+	// CPU ----------------------------------------------------
+	for (int r = 0; r < arraySize; r++) {
+		b[r] = a[r];
+	}
+
+	bubbleSortCpu(b, arraySize);
+
+	if (display) {
+		cout << "B" << endl;
+		for (int j = 0; j < arraySize; j++) {
+			cout << b[j] << "\t";
+		}
+		cout << endl;
+	}
+
+	for (int r = 0; r < arraySize; r++) {
+		b[r] = 0;
+	}
+
+	// GPU1 ---------------------------------------------------
+	bubbleSortCuda(a, b, arraySize);
+
+	if (display) {
+		cout << "B" << endl;
+		for (int j = 0; j < arraySize; j++) {
+			cout << b[j] << "\t";
+		}
+		cout << endl;
+	}
+
+	for (int r = 0; r < arraySize; r++) {
+		b[r] = 0;
+	}
+
+	// GPU2 ---------------------------------------------------
+	bubbleSortCuda2(a, b, arraySize);
+
+	if (display) {
+		cout << "B" << endl;
+		for (int j = 0; j < arraySize; j++) {
+			cout << b[j] << "\t";
+		}
+		cout << endl;
+	}
+
+	for (int r = 0; r < arraySize; r++) {
+		b[r] = 0;
+	}
+
+	// GPU3 ---------------------------------------------------
+	bubbleSortCuda3(a, b, arraySize);
+
+	if (display) {
+		cout << "B" << endl;
+		for (int j = 0; j < arraySize; j++) {
+			cout << b[j] << "\t";
+		}
+		cout << endl;
+	}
+
+	// END ----------------------------------------------------
+
+    checkCuda(cudaDeviceReset());
+    
+	cout << "DONE";
+	cin.ignore();
 
     return 0;
-}
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
 }
